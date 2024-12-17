47fb723b4b3183e42d2ee6202e1994db59990df4
Surajit Singha Sisir <sci.surajit@gmail.com>
2024-12-17 21:59:09 +0600

    Re Export The ShowToast Function in main js function
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- mastors.js
+++ mastors.js
@@ -2,5 +2,10 @@
 import "./JS/combobox.js";
 import "./JS/form.js";
 import "./JS/tooltip.js";
-import "./navbars/nav-1.js"
-import "./JS/showtoast.js"
+import "./navbars/nav-1.js";
+import "./JS/showtoast.js";
+
+import { showToast } from "./JS/showtoast.js"; // Import the function
+
+// Re-export the showToast function
+export { showToast };
